50db13684a9dfbdcb7e6b6dc3de9dcb52fd77ccc
sahory16 <git-sync@trilby.media>
2019-06-03 00:21:45 -0500

    (Grav GitSync) Automatic Commit from sahory16

--- plugins/adminidenticons/vendor/ranvis/identicon/src/ITile.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @author SATO Kentaro
- * @license BSD 2-Clause License
- */
-
-// ITlie is deprecated (without @-annotation)
-class_alias('Ranvis\Identicon\TileInterface', 'Ranvis\Identicon\ITile');
